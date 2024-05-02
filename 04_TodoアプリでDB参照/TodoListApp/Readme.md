1d79b80a0f2cdc2a4dc5bc6d788a272524b4bdff
prota-p <112764033+prota-p@users.noreply.github.com>
2024-05-02 17:50:11 +0900

    Added 04

---
+++ 04_TodoアプリでDB参照/TodoListApp/Readme.md
@@ -0,0 +1,10 @@
+# 【C#、Blazor】Webアプリ開発入門編（4）のサンプルコード
+## 記事
+- [こちら](https://prota-p.com/csharp_web_blazor4_todo_ref/)の記事を参考にしてください。
+## セットアップ手順
+- このサンプルには、sqliteファイル（`Data`フォルダ配下の`app.db`）が含まれていません。
+- そのため、最初に以下のコマンドで`app.db`を作成します。
+    ```
+    $ dotnet ef database update
+    ```
+- DB Browser for SQLiteなどを用いてでテスト用のDBレコードを追加してからアプリを実行してください。
---
+++ 04_TodoアプリでDB参照/TodoListApp/Services/TodoListService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListApp.Data;
+using TodoListApp.Models;
+
+namespace TodoListApp.Services;
+
+public class TodoListService
+{
+    private readonly ApplicationDbContext _context;
+
+    public TodoListService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<TodoItem>> GetTodosAsync()
+    {
+        var items = await _context.TodoItems.ToListAsync();
+        return items;
+    }
+}
