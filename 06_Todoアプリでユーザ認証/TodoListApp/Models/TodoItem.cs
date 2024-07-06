f8e07bd0959bcdd5f9e03fe403ef20844cd100c7
prota-p <112764033+prota-p@users.noreply.github.com>
2024-07-06 17:21:11 +0900

    Added Blazor Ex6

---
+++ 06_Todoアプリでユーザ認証/TodoListApp/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TodoListApp.Data;
+
+namespace TodoListApp.Models;
+
+public class TodoItem
+{
+    [Key]
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    [ForeignKey(nameof(ApplicationUser))]
+    public string UserId { get; set; } = string.Empty;
+}
